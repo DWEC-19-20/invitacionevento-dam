6b8311266d5045e749d77012907c6227a93591da
unknown <deschouwer.pablo@gmail.com>
2020-01-29 09:16:38 +0100

    CommitF

--- Codiguito.js
+++ Codiguito.js
@@ -1,6 +1,4 @@
-document.body.onload = addElement;
-
-function borrar(elemento){
+/*function borrar(elemento){
     {
         var id=elemento.parentNode.getAttribute("li");
         node=document.getElementById(li);
@@ -16,13 +14,28 @@ let spanamodificar=document.getElementById("nombrecito");
 document.getElementById("nombrecito").reset();
 spanamodificar=document.createTextNode("");
 }
+*/
 
+//document.getElementById("registrar").addEventListener("submit",añadir());
 
-document.getElementById("registrar").addEventListener("click",añadir());
+document.getElementById("añadirNombre").addEventListener("click",añadir);
 
 function añadir(name){
+  event.preventDefault();
 
-event.preventDefault();
+  let invitado=document.getElementById("nombreAñadir").value;
+  for (let c=0; c<document.getElementsByTagName("span").length;c++){
+    if (invitado==document.getElementsByTagName("span")[c].innerHTML){
+      document.getElementById("d").style.display="block";
+      document.getElementById("d").innerHTML="No se pueden repetir nombres";
+      return false
+    }
+    else{
+      document.getElementById("d").style.display="none";
+    }
+  }
+
+  
 
   let newLi=document.createElement("li");
 
@@ -34,6 +47,7 @@ event.preventDefault();
   let newTextito=document.createElement("input");
   newTextito.type="text";
   newTextito.style.display="none";
+  
 
   let newLabel=document.createElement("label");
   newLi.appendChild(newLabel); //añadir Labbel a Li
@@ -43,8 +57,9 @@ event.preventDefault();
   newContent=document.createElement("input");
   newLabel.appendChild(newContent); //Añadir Input a Li
   newContent.type="checkbox";
+  newLabel.parentElement.className="not-responded";
   newLabel.appendChild(newContent);
-  
+
   newContent.addEventListener('change',function(){
     if (this.checked){
       newLabel.parentElement.className="responded"
@@ -53,17 +68,39 @@ event.preventDefault();
       newLabel.parentElement.className="not-responded"
     }
     });
+  
+  
+    document.getElementById("añadirNombre").addEventListener("click", function(){
+      let lista=document.getElementById("invitedList");
+      /*let confirmados=lista.getElementsByClassName("not-responded");
+      let n=lista.getElementsByClassName("responded");
+      let nombre=confirmados.item(0).firstChild.textContent;*/
+      for (let i=0; lista;i++){
+            if (lista.item(i).firstChild.firstChild.textContent==nombre){
+              document.getElementById("d").innerHTML="Error, no puede poner dos nombre iguales";
+              nombre=confirmados.item(i);
+            }
+    }})
     
-
   let newButton=document.createElement("button");
   newLi.appendChild(newButton); //Añadir Button a Li
   newContent=document.createTextNode("edit");
   newButton.appendChild(newContent);
 
-  newButton.addEventListener("click",function(){    
-    let nom=(document.getElementById("nombreAñadir").value);
-    //var nombre=document.getElementById("nombreAÑadir").value;
-    newButton.parentElement.firstChild.innerText = nom;
+  newButton.addEventListener("click",function(){ 
+    
+    let nombrecillo=window.prompt("Nombre a añadir");
+    for (let i=0; c<document.getElementsByTagName("span").length;i++){
+      if (nombrecillo==document.getElementsByTagName("span")[i].innerHTML){
+        document.getElementById("d").style.display="block";
+        document.getElementById("d").innerHTML="No se pueden repetir nombres";
+        return false
+      } 
+      else{ 
+    document.getElementById("d").style.display="none";
+    newButton.parentElement.firstChild.innerText = nombrecillo;
+      }
+    }   
   });
 
   let newButton2=document.createElement("button");
@@ -72,8 +109,9 @@ event.preventDefault();
   newButton2.appendChild(newContent);
 
   newButton2.addEventListener("click", function(){
-    window.confirm("seguro de querer borrar?");
-    if (window.confirm=true){
+    let res=window.confirm("seguro de querer borrar?");
+    console.log(res)
+    if (res=true){
       newLi.remove();
   }
   });
@@ -81,10 +119,33 @@ event.preventDefault();
 // añade el elemento creado y su contenido al DOM 
   let currentLi=document.getElementById("li");
   document.getElementById("invitedList").insertBefore(newLi, currentLi);
-
   document.getElementById("registrar").reset();
-
-  
-  
 }
+
+document.getElementById("confirmados").addEventListener("change", function(){
+  let lista=document.getElementById("invitedList");
+  let confirmados=lista.getElementsByClassName("not-responded");
+  if (this.checked){
+    for (let i=0; confirmados;i++){
+      try {
+        confirmados.item(i).setAttribute("style","display:none;");
+      } catch (error) {
+        break
+      }
+      
+    }
+  }
+  else {
+    for (let i=0; confirmados;i++){
+      try {
+        confirmados.item(i).setAttribute("style","display:block;");
+      } catch (error) {
+        break
+      }
+      
+    }
+    }
+  }
+)
+
 
